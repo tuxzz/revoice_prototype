a8e4e0802c501e97db8b13bcaf6765a7e19a1ad1
Otonashi Hikaria <xir.hikaria@gmail.com>
2016-10-04 10:14:01 +0800

    crlf->lf

--- revoice/__init__.py
+++ revoice/__init__.py
@@ -5,5 +5,8 @@ from . import pyin
 from . import monopitch
 from . import mononote
 from . import adaptivestft
+from . import melenvelope
+from . import trueenvelope
+from . import mfienvelope
 
 __all__ = ["common", "sparsehmm", "yin", "pyin", "monopitch", "mononote", "adaptivestft", "melenvelope", "trueenvelope", "mfienvelope"]
