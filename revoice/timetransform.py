76965381916d7151a2073820964943fb88478511
otonashi1991 <xir.hikaria@gmail.com>
2017-06-09 10:03:35 +0800

    fix for unvoiced additional parameter

--- revoice/timetransform.py
+++ revoice/timetransform.py
@@ -7,25 +7,36 @@ class Processor:
     def __init__(self, sr):
         self.samprate = float(sr)
 
-    def simple(self, timeList, f0List, hPhaseList = None, additional = ()):
+    def simple(self, timeList, f0List, hPhaseList = None, vAdditional = (), vuvAdditional = ()):
         nHop = len(f0List)
         nNewHop = len(timeList)
 
         newF0List = np.zeros(nNewHop)
         if(hPhaseList is not None):
             newHPhaseList = np.zeros((nNewHop, hPhaseList.shape[1]))
-        newAdditional = []
-        additional = list(additional)
-        for iItem, item in enumerate(additional):
+        newVAdditional, newVUVAdditional = [], []
+        vAdditional, vuvAdditional = list(vAdditional), list(vuvAdditional)
+        for iItem, item in enumerate(vAdditional):
             if(isinstance(item, tuple)):
                 assert(len(item) == 2)
                 item, default = item
-                additional[iItem] = item
+                vAdditional[iItem] = item
             else:
                 default = 0.0
             newItem = np.full((nNewHop, *item.shape[1:]), default)
-            newAdditional.append(newItem)
+            newVAdditional.append(newItem)
 
+        for iItem, item in enumerate(vuvAdditional):
+            if(isinstance(item, tuple)):
+                assert(len(item) == 2)
+                item, default = item
+                vuvAdditional[iItem] = item
+            else:
+                default = 0.0
+            newItem = np.full((nNewHop, *item.shape[1:]), default)
+            newVUVAdditional.append(newItem)
+
+        # v
         segments = splitArray(f0List)
         segBeginHop = 0
         newHopIndexList = np.arange(nNewHop)
@@ -39,15 +50,20 @@ class Processor:
                     newF0List[newHopIndexBegin:newHopIndexEnd] = f0List[iHop]
                     if(hPhaseList is not None):
                         newHPhaseList[newHopIndexBegin:newHopIndexEnd] = hPhaseList[iHop]
-                    for iItem, item in enumerate(additional):
-                        newAdditional[iItem][newHopIndexBegin:newHopIndexEnd] = item[iHop]
+                    for iItem, item in enumerate(vAdditional):
+                        newVAdditional[iItem][newHopIndexBegin:newHopIndexEnd] = item[iHop]
                 else:
                     iplX = np.linspace(segBeginHop, segEndHop + 1, segEndHop - segBeginHop)
                     iplY = timeList[newHopIndexBegin:newHopIndexEnd]
                     newF0List[newHopIndexBegin:newHopIndexEnd] = ipl.interp1d(iplX, f0List[segBeginHop:segEndHop], kind = 'linear')(iplY)
                     if(hPhaseList is not None):
                         newHPhaseList[newHopIndexBegin:newHopIndexEnd] = ipl.interp1d(iplX, np.unwrap(hPhaseList[segBeginHop:segEndHop], axis = 0), axis = 0, kind = 'linear')(iplY)
-                    for iItem, item in enumerate(additional):
-                        newAdditional[iItem][newHopIndexBegin:newHopIndexEnd] = ipl.interp1d(iplX, item[segBeginHop:segEndHop], axis = 0, kind = 'linear')(iplY)
+                    for iItem, item in enumerate(vAdditional):
+                        newVAdditional[iItem][newHopIndexBegin:newHopIndexEnd] = ipl.interp1d(iplX, item[segBeginHop:segEndHop], axis = 0, kind = 'linear')(iplY)
             segBeginHop += len(segment)
-        return (newF0List, newHPhaseList, *newAdditional)
+        del iplX, iplY
+
+        # vuv
+        for iItem, item in enumerate(vuvAdditional):
+            newVUVAdditional[iItem][:] = ipl.interp1d(np.arange(nHop), item, axis = 0, kind = 'linear')(timeList)
+        return newF0List, newHPhaseList, newVAdditional, newVUVAdditional
